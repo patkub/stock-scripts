3a16027247a1e1308ca85a731872cc4feb34637c
Patrick Kubiak <pk9948@rit.edu>
2021-05-24 12:49:06 -0400

    consider buying/selling

--- finviz-screener/main.py
+++ finviz-screener/main.py
@@ -1,8 +1,10 @@
 from operator import itemgetter
 from finviz.screener import Screener
 
+
 def print_stock(stock):
     print("{ticker} \t ${price} \t RSI: {rsi}".format(ticker = stock['Ticker'], price = stock['Price'], rsi = stock['RSI']))
+
 
 #https://finviz.com/screener.ashx?v=121&f=cap_mega&o=-marketcap
 
@@ -12,31 +14,49 @@ filters = ['exch_any', 'cap_mega']
 # Get the technical table and sort it by marketcap descending
 stock_list = Screener(filters=filters, table='Technical', order='-marketcap')
 
+
 # Print tickers
 print("\nTop 200 Billion Plus Market Cap Companies:\n")
 tickers = [stock['Ticker'] for stock in stock_list]
 for a,b,c,d,e in zip(tickers[::5],tickers[1::5],tickers[2::5],tickers[3::5],tickers[4::5]):
     print('{:<20}{:<20}{:<20}{:<20}{:<}'.format(a,b,c,d,e))
 
+
 # RSI
 stock_list_by_rsi = sorted(stock_list.data, key=lambda x: float(x['RSI']))
 stock_list_by_rsi_rev = sorted(stock_list.data, key=lambda x: float(x['RSI']), reverse=True)
-stock_list_rsi_under = dict()
-rsi_under_list = ['50', '45', '40', '35']
 
-for rsi in rsi_under_list:
-    stock_list_rsi_under[rsi] = list(filter(lambda x : float(x['RSI']) < float(rsi), stock_list_by_rsi))
+rsi_lists = {
+    'values': {
+        'under' : ['50', '45', '40', '35'],
+        'above' : ['70', '60']
+    },
+    'data': {
+        'under' : dict(),
+        'above' : dict()
+    },
+}
 
-for rsi in rsi_under_list:
-    print("\nRSI under {rsi}:\n".format(rsi = rsi))
-    for stock in stock_list_rsi_under[rsi]:
-        print_stock(stock)
+for rsi in rsi_lists['values']['under']:
+    rsi_lists['data']['under'][rsi] = list(filter(lambda x : float(x['RSI']) < float(rsi), stock_list_by_rsi))
 
-stock_list_rsi_above_60 = list(filter(lambda x : float(x['RSI']) > float(60), stock_list_by_rsi_rev))
+for rsi in rsi_lists['values']['above']:
+    rsi_lists['data']['above'][rsi] = list(filter(lambda x : float(x['RSI']) > float(rsi), stock_list_by_rsi))
 
-print("\nRSI above 60:\n")
-for stock in stock_list_rsi_above_60:
-    print_stock(stock)
+rsi_list_types = ['under', 'above']
+for type in rsi_list_types:
+    for rsi in rsi_lists['values'][type]:
+        print("\nRSI {type} {rsi}:\n".format(type = type, rsi = rsi))
+        for stock in rsi_lists['data'][type][rsi]:
+            print_stock(stock)
+
+
+# consider buying/selling
+tickers_rsi_under_45 = [stock['Ticker'] for stock in rsi_lists['data']['under']['45']]
+tickers_rsi_above_60 = [stock['Ticker'] for stock in rsi_lists['data']['above']['60']]
+
+print("\nConsider buying (RSI under 45): {}".format(", ".join(tickers_rsi_under_45)))
+print("\nConsider selling (RSI above 60): {}".format(", ".join(tickers_rsi_above_60)))
 
 # blank line at end
 print()
